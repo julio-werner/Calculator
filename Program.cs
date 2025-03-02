048d001dc70ba7b7f21f8ffca52b46f0efdb587f
julio-werner <j1102wernerj11@gmail.com>
2025-03-02 15:58:50 -0300

    Soma

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual o primeiro valor? ");
+            float Number1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o segundo valor? ");
+            float Number2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("_________________________");
+            Console.WriteLine();
+
+            Console.WriteLine("O resultado da soma é " + (Number1 + Number2));
+        }
+        
+    }
+}
