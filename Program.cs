3133592762dc66d90467d44f0a6539f0f83bb58b
julio-werner <j1102wernerj11@gmail.com>
2025-03-02 16:02:11 -0300

    Subtração

--- Program.cs
+++ Program.cs
@@ -24,6 +24,22 @@ namespace Calculator
 
             Console.WriteLine("O resultado da soma é " + (Number1 + Number2));
         }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual o primeiro valor? ");
+            float Number1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o segundo valor? ");
+            float Number2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("_________________________");
+            Console.WriteLine();
+
+            Console.WriteLine("O resultado da soma é " + (Number1 - Number2));
+        }
         
     }
 }
