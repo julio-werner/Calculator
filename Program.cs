09796991cf3845cdb09799b94e5dd6fb56b71b67
julio-werner <j1102wernerj11@gmail.com>
2025-03-02 16:51:56 -0300

    Menu do Usuario/Melhor interação

--- Program.cs
+++ Program.cs
@@ -7,6 +7,40 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            MenuUsuario();
+        }
+
+        static void MenuUsuario()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Bem-Vindo a minha caculadora com C#, qual operalção deseja realizar?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair :()");
+
+            Console.WriteLine("________________________________________");
+            Console.WriteLine("");
+
+            Console.WriteLine("Selecione uma opção: ");
+
+            short Escolha = short.Parse(Console.ReadLine());
+
+            switch (Escolha)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: Console.WriteLine("Obrigado por usar a calculadora!"); 
+                Environment.Exit(0); break;
+                default:
+                    Console.WriteLine("Você digitou uma opção invalida, por favor veja as opções válidas!");
+                    Console.WriteLine("Pressione qualquer tecla para continuar.");
+                    Console.ReadKey(); MenuUsuario(); break;
+            }
         }
 
         static void Soma()
@@ -23,8 +57,8 @@ namespace Calculator
             Console.WriteLine();
 
             Console.WriteLine("O resultado da soma é: " + (Number1 + Number2));
+            Continuar();
         }
-
         static void Subtracao()
         {
             Console.Clear();
@@ -39,6 +73,7 @@ namespace Calculator
             Console.WriteLine();
 
             Console.WriteLine("O resultado da subtração é: " + (Number1 - Number2));
+            Continuar();
         }
         static void Divisao()
         {
@@ -54,6 +89,7 @@ namespace Calculator
             Console.WriteLine();
 
             Console.WriteLine("O resultado da divisão é: " + (Number1 / Number2));
+            Continuar();
         }
         static void Multiplicacao()
         {
@@ -69,7 +105,25 @@ namespace Calculator
             Console.WriteLine();
 
             Console.WriteLine("O resultado da multiplicação é: " + (Number1 * Number2));
+            Continuar();
         }
-        
+        static void Continuar()
+        {
+            Console.WriteLine("Deseja realizar outra operação? (S/N)");
+
+            string resposta = Console.ReadLine().ToUpper();
+
+            if (resposta == "S")
+            {
+                MenuUsuario();
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Obrigado por usar a calculadora!");
+                Environment.Exit(0);
+            }
+        }
+
     }
 }
