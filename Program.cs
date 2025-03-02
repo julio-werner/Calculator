00fb02c3b3ee93c8d4f0693ae152acccda95a29f
julio-werner <j1102wernerj11@gmail.com>
2025-03-02 16:15:24 -0300

    Divisão

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace Calculator
             Console.WriteLine("_________________________");
             Console.WriteLine();
 
-            Console.WriteLine("O resultado da soma é " + (Number1 + Number2));
+            Console.WriteLine("O resultado da soma é: " + (Number1 + Number2));
         }
 
         static void Subtracao()
@@ -38,7 +38,22 @@ namespace Calculator
             Console.WriteLine("_________________________");
             Console.WriteLine();
 
-            Console.WriteLine("O resultado da soma é " + (Number1 - Number2));
+            Console.WriteLine("O resultado da subtração é: " + (Number1 - Number2));
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual o primeiro valor? ");
+            float Number1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o segundo valor? ");
+            float Number2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("_________________________");
+            Console.WriteLine();
+
+            Console.WriteLine("O resultado da divisão é: " + (Number1 / Number2));
         }
         
     }
